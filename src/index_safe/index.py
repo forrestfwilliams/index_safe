e56290937b92da2679587956f18951d8b7c92a46
Forrest Williams <ffwilliams2@alaska.edu>
2023-01-31 19:03:14 -0600

    can create gzidx for zip member

--- src/index_safe/index.py
+++ src/index_safe/index.py
@@ -1,11 +1,18 @@
+import io
 import struct
+import tempfile
+import zipfile
 from pathlib import Path
 
 import indexed_gzip as igzip
 import numpy as np
 
+from index_safe import index_safe, utils
 
-def create_gzidx(gz_path, spacing=2**18):
+ZIP_PATH = 'S1A_IW_SLC__1SDV_20200604T022251_20200604T022318_032861_03CE65_7C85.zip'
+
+
+def create_gzidx(gz_path, spacing=2**20):
     out_path = Path(gz_path).with_suffix('.gzidx')
 
     with igzip.IndexedGzipFile(gz_path, spacing=spacing) as f:
@@ -23,7 +30,8 @@ def parse_gzidx(fobj):
     fobj.seek(0)
     return parsed
 
-def build_gzidx(input_gzidx_path, output_gzidx_path, points):
+
+def build_gzidx(input_gzidx_path, output_gzidx_path, points=None, filesize=None, compressed_offset=0):
     header_length = 35
     point_size = 18
 
@@ -33,34 +41,66 @@ def build_gzidx(input_gzidx_path, output_gzidx_path, points):
 
     window_size = struct.unpack('<I', data[27:31])[0]
     n_points = struct.unpack('<I', data[31:35])[0]
-
     points_length = point_size * n_points
-    point_offsets = np.arange(0, (n_points) * point_size, point_size) + header_length
 
     # FIXME Assume only first window entry is zero
     window_offsets = np.arange(0, array[1:].shape[0] * window_size, window_size) + header_length + points_length
     window_offsets = np.append([0], window_offsets, axis=0)
 
-    indexes = np.where(np.isin(array[:, 0], points))[0].tolist()
+    array = np.append(array, np.expand_dims(window_offsets, axis=1), axis=1)
+    if points:
+        array = array[np.isin(array[:, 0], points), :].copy()
+    array[:, 0] += compressed_offset
+
     point_bytes = []
     window_bytes = []
-    for i in indexes:
-        point_entry = data[point_offsets[i] : point_offsets[i] + point_size]
+    for row in array:
+        point_entry = struct.pack('<QQBB', *row[:4].tolist())
 
-        if window_offsets[i] == 0:
+        if row[4] == 0:
             window_entry = b''
         else:
-            window_entry = data[window_offsets[i] : window_offsets[i] + window_size]
+            window_entry = data[row[4] : row[4] + window_size]
 
         point_bytes.append(point_entry)
         window_bytes.append(window_entry)
+    
+    if filesize:
+        filesize_bytes = struct.pack('<Q', filesize)
+    else:
+        filesize_bytes = data[7:15]
 
-    out_gzidx = data[:31] + struct.pack('<I', len(points)) + b''.join(point_bytes) + b''.join(window_bytes)
-
+    header = data[:7] + filesize_bytes + data[15:31] + struct.pack('<I', array.shape[0])
+    out_gzidx = header + b''.join(point_bytes) + b''.join(window_bytes)
     with open(output_gzidx_path, 'wb') as fobj:
         fobj.write(out_gzidx)
 
     return output_gzidx_path
+
+
+def create_gzidx_for_zip_member(zip_path, member_name, out_path):
+    gz_header_length = 10
+    with zipfile.ZipFile(zip_path) as f:
+        zinfo = [x for x in f.infolist() if member_name in Path(x.filename).name][0]
+
+    offset_zinfo = utils.OffsetZipInfo(zip_path, zinfo)
+    offset = offset_zinfo.get_compressed_offset()
+    archive_size = Path(ZIP_PATH).stat().st_size
+
+    with open(zip_path, 'rb') as f:
+        f.seek(offset.start)
+        body = f.read(offset.stop - offset.start)
+
+    gz_body = index_safe.wrap_as_gz(body, offset_zinfo)
+    tmp = tempfile.NamedTemporaryFile()
+    with igzip.IndexedGzipFile(io.BytesIO(gz_body), spacing=2**22) as f:
+        f.build_full_index()
+        f.export_index(tmp.name)
+
+    adjusted_offset = offset.start - gz_header_length
+    build_gzidx(tmp.name, out_path, filesize=archive_size, compressed_offset=adjusted_offset)
+    return out_path
+
 
 if __name__ == '__main__':
     gz_path = 's1a-iw2-slc-vv-20200604t022253-20200604t022318-032861-03ce65-005.tiff.gz'
--- tests/test_index.py
+++ tests/test_index.py
@@ -1,24 +1,29 @@
+import io
 import tempfile
+import zipfile
 from pathlib import Path
 
 import indexed_gzip as igzip
 import numpy as np
 import pytest
 
-from index_safe import index
+from index_safe import index, index_safe, utils
 
 GZ_PATH = 's1a-iw2-slc-vv-20200604t022253-20200604t022318-032861-03ce65-005.tiff.gz'
 GZIDX_PATH = 's1a-iw2-slc-vv-20200604t022253-20200604t022318-032861-03ce65-005.tiff.gzidx'
+ZIP_PATH = 'S1A_IW_SLC__1SDV_20200604T022251_20200604T022318_032861_03CE65_7C85.zip'
+TIFF_PATH = 's1a-iw2-slc-vv-20200604t022253-20200604t022318-032861-03ce65-005.tiff'
 
 
 @pytest.fixture(scope='module')
 def seek_point_array():
     if not Path(GZIDX_PATH).exists():
-        with igzip.IndexedGzipFile(GZ_PATH, spacing=int(4194304 / 8)) as f:
+        print('Creating full gzidx')
+        with igzip.IndexedGzipFile(GZ_PATH, spacing=2**22) as f:
             f.build_full_index()
             f.export_index(GZIDX_PATH)
 
-    with igzip.IndexedGzipFile(GZ_PATH, spacing=int(4194304 / 8)) as f:
+    with igzip.IndexedGzipFile(GZ_PATH, spacing=2**22) as f:
         f.import_index(GZIDX_PATH)
         seek_points = list(f.seek_points())
     array = np.array(seek_points)
@@ -33,7 +38,7 @@ def test_parse_gzidx(seek_point_array):
 
 
 def test_build_gzidx(seek_point_array):
-    points = [100,105]
+    points = [10,15]
     points_compressed = seek_point_array[points, 1].tolist()
     points_uncompressed = seek_point_array[points, 0].tolist()
 
@@ -49,5 +54,26 @@ def test_build_gzidx(seek_point_array):
         fobj.import_index(tmp.name)
         fobj.seek(points_uncompressed[0] + 1)
         test = fobj.read(points_uncompressed[1] - 1)
+
+    assert golden == test
+
+
+def test_create_gzidx_for_zip_member():
+    start = 10
+    length = 15
+
+    with zipfile.ZipFile(ZIP_PATH) as f:
+        zinfo = [x for x in f.infolist() if TIFF_PATH in Path(x.filename).name][0]
+        with f.open(zinfo.filename, 'r') as member:
+            member.seek(start)
+            golden = member.read(length)
+
+
+    tmp = tempfile.NamedTemporaryFile()
+    index.create_gzidx_for_zip_member(ZIP_PATH, TIFF_PATH, tmp.name)
+    with igzip.IndexedGzipFile(ZIP_PATH, skip_crc_check=True) as f:
+        f.import_index(tmp.name)
+        f.seek(start)
+        test = f.read(length)
 
     assert golden == test
