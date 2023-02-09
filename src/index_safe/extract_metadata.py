5aa02f837c4af4c86133bc5b084af4196c8fe8d7
Forrest Williams <ffwilliams2@alaska.edu>
2023-02-09 15:16:23 +0000

    enable s3 direct access for metadata download

--- src/index_safe/extract_metadata.py
+++ src/index_safe/extract_metadata.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 from concurrent.futures import ThreadPoolExecutor
 from itertools import repeat
+from pathlib import Path
 
 import boto3
 import botocore
@@ -21,31 +22,18 @@ MB = 1024 * KB
 MAX_WBITS = 15
 
 
-def s3_download(bucket: str, key: str, range_header: str) -> bytes:
-    client = boto3.client('s3')
-    resp = client.get_object(Bucket=bucket, Key=key, Range=range_header)
-    body = resp['Body'].read()
-    return body
+def extract_bytes(
+    url: str, offset: utils.Offset, client: botocore.client.BaseClient | requests.sessions.Session
+) -> bytes:
+    range_header = f'bytes={offset.start}-{offset.stop - 1}'
 
-
-def extract_bytes_s3(bucket: str, key: str, metadata: utils.BurstMetadata) -> bytes:
-    range_header = f'bytes={metadata.compressed_offset.start}-{metadata.compressed_offset.stop}'
-    body = s3_download(bucket, key, range_header)
-    body = zlib.decompressobj(-1 * MAX_WBITS).decompress(body)
-    return body
-
-
-def http_download(url: str, range_header: str) -> bytes:
-    with requests.Session() as s:
-        resp = s.get(url, headers={'Range': range_header})
-        resp.raise_for_status()
+    if isinstance(client, botocore.client.BaseClient):
+        resp = client.get_object(Bucket=utils.BUCKET, Key=Path(url).name, Range=range_header)
+        body = resp['Body'].read()
+    elif isinstance(client, requests.sessions.Session):
+        resp = client.get(url, headers={'Range': range_header})
         body = resp.content
-    return body
 
-
-def extract_bytes_http(url: str, offset: utils.Offset) -> bytes:
-    range_header = f'bytes={offset.start}-{offset.stop}'
-    body = http_download(url, range_header)
     body = zlib.decompressobj(-1 * zlib.MAX_WBITS).decompress(body)
     return body
 
@@ -56,14 +44,25 @@ def row_to_metadata_entry(row: pd.Series) -> utils.XmlMetadata:
     return metadata_entry
 
 
-def extract_metadata(slc_name, df_file_name):
+def extract_metadata(slc_name, df_file_name, strategy='s3'):
     url = utils.get_download_url(slc_name)
     df = pd.read_csv(df_file_name)
     slc_df = df.loc[df.slc == slc_name]
     offsets = [row_to_metadata_entry(row).offset for i, row in slc_df.iterrows()]
 
+    if strategy == 's3':
+        creds = utils.get_credentials()
+        client = boto3.client(
+            "s3",
+            aws_access_key_id=creds["accessKeyId"],
+            aws_secret_access_key=creds["secretAccessKey"],
+            aws_session_token=creds["sessionToken"],
+        )
+    elif strategy == 'http':
+        client = requests.session()
+
     with ThreadPoolExecutor(max_workers=20) as executor:
-        results = list(tqdm(executor.map(extract_bytes_http, repeat(url), offsets), total=len(offsets)))
+        results = list(tqdm(executor.map(extract_bytes, repeat(url), offsets, repeat(client)), total=len(offsets)))
 
     content = b''.join(results)
     with open(f'{slc_name}.xml', 'wb') as f:
